b63874978bfdd7117c289c4c31240b59d7d3eaf3
New_2000 <TanachotNew2543@gmail.com>
2024-03-05 01:16:19 +0700

    add leetcode knowledge

--- main.go
+++ main.go
@@ -1,31 +1,91 @@
 package main
 
 import (
-	// "encoding/json"
-
 	"fmt"
 	"strings"
-	"sync"
 )
+
+type Covid struct {
+	ConfirmDate    string
+	No             int
+	Age            *int
+	Gender         string
+	GenderEn       string
+	Nation         string
+	NationEn       string
+	Province       string
+	ProvinceId     int
+	District       string
+	ProvinceEn     string
+	StatQuarantine int
+}
+
+type tets struct {
+	Data []Covid
+}
 
 func main() {
 
-	var waitGroup sync.WaitGroup
-	var mutex sync.Mutex
-	var str []string
+	// a := make([]string, 0, 5)
+	// b := []string{}
 
-	for i := 0; i < 5; i++ {
-		waitGroup.Add(1)
-		go func() {
-			defer waitGroup.Done()
-			mutex.Lock()
-			str = append(str, "*")
-			mutex.Unlock()
-		}()
-	}
+	// a = append(a, "1", "2", "3", "4", "5", "6")
+	// b = append(b, "1", "2")
 
-	waitGroup.Wait()
-	fmt.Println(str)
+	// fmt.Println(a, cap(a))
+	// fmt.Println(b, cap(b))
+
+	// fmt.Println("start")
+	// defer fmt.Println("nndsa;dkakljbkaebkd")
+	// log.Fatal("this is fatal")
+	// fmt.Println("never happen")
+
+	// response, err := http.Get("https://static.wongnai.com/devinterview/covid-cases.json")
+
+	// if err != nil {
+	// 	// return nil, err
+	// 	fmt.Println(err)
+	// }
+
+	// body, err := io.ReadAll(response.Body)
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// result := new(tets)
+
+	// if err = json.Unmarshal(body, result); err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// a := 0
+	// for _, i := range result.Data {
+	// 	// fmt.Println(i.Age)
+	// 	if i.Age != nil && *i.Age == 0 {
+	// 		a++
+	// 		fmt.Println(i)
+	// 	}
+	// }
+
+	// println(a)
+
+	// var waitGroup sync.WaitGroup
+	// var mutex sync.Mutex
+	// var str []string
+
+	// for i := 0; i < 5; i++ {
+	// 	waitGroup.Add(1)
+	// 	go func() {
+	// 		defer waitGroup.Done()
+	// 		mutex.Lock()
+	// 		str = append(str, "*")
+	// 		mutex.Unlock()
+	// 	}()
+	// }
+
+	// waitGroup.Wait()
+	// fmt.Println(str)
 
 	// fmt.Println(time.Now().Unix())
 
@@ -63,6 +123,110 @@ func main() {
 	// }
 
 	// fmt.Println("")
+}
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	newList := new(ListNode)
+	head := newList
+
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			newList.Next = list1
+			list1 = list1.Next
+		} else {
+			newList.Next = list2
+			list2 = list2.Next
+		}
+
+		newList = newList.Next
+	}
+
+	if list1 == nil {
+		newList.Next = list2
+	} else {
+		newList.Next = list1
+	}
+
+	return head.Next
+}
+
+// to do
+func searchInsert_LogN(nums []int, target int) int {
+	return 0
+}
+
+func searchInsert_ON(nums []int, target int) int {
+	index := 0
+	for i, v := range nums {
+		if v >= target {
+			return i
+		}
+
+		index++
+	}
+
+	return index
+}
+
+func isValidMap(s string) bool {
+	m := map[rune]rune{
+		'}': '{',
+		')': '(',
+		']': '[',
+	} // can use number of char in ascii table instead of hashmap
+
+	r := []rune{}
+	for _, i := range s {
+		println(string(m[i]))
+		if v := m[i]; v != 0 {
+			l := len(r)
+			if l != 0 {
+				if v == r[l-1] {
+					r = r[:l-1]
+					continue
+				}
+			}
+
+			return false
+		}
+
+		r = append(r, i)
+		println(string(r))
+	}
+
+	if len(r) != 0 {
+		return false
+	}
+
+	return true
+}
+
+func isValid(s string) bool {
+	if len(s) == 0 || len(s)%2 == 1 {
+		return false
+	}
+
+	r := []rune{}
+	for _, v := range s {
+		if l := len(r); l == 0 {
+			r = append(r, v)
+		} else {
+
+			if j := l - 1; v-1 == r[j] || v-2 == r[j] {
+				r = r[:j]
+			} else {
+				r = append(r, v)
+			}
+		}
+	}
+
+	return len(r) == 0
 }
 
 func twoSum(nums []int, target int) []int {
